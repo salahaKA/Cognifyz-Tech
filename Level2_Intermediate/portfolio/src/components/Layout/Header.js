2ef7e7c40ced374b31f9a09bee8a57aca5561d5b
Kadeejath Salaha <codestudy475@gmail.com>
2024-08-17 01:10:01 +0530

    Header Ui animation  modified

--- Level2_Intermediate/portfolio/src/components/Layout/Header.js
+++ Level2_Intermediate/portfolio/src/components/Layout/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import '../../styles/Header.css';
+import { Home, Home as HomeIcon } from '@mui/icons-material'
 
 const Header = () => {
   return (
     <AppBar position="static" className="appBar">
-      <Toolbar>
+      <Toolbar className='toolbar'>
+        <IconButton edge="start" color="inherit" aria-label='logo' className='logo'>
+          <HomeIcon fontSize='large'></HomeIcon>
+        </IconButton>
+        
         <Typography variant="h6" className="title">
           My Portfolio
         </Typography>
